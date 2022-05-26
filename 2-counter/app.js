49faff964cf4885eeda7a8afa9937aa2d33b47f6
Ich1ro <pavel.dubenchuk.02@gmail.com>
2022-05-26 20:35:17 +0300

    refactoring

--- 2-counter/app.js
+++ 2-counter/app.js
@@ -6,9 +6,11 @@ btns.forEach(function(btn){
     btn.addEventListener('click', function(e){
         const payload = e.target.dataset.payload;
         count = payload ? count + +payload : 0;
+
         value.style.color = count > 0 ? 'green' :
             count < 0 ? 'red' :
                 '#222222';
+
         value.textContent = count
     })
 })
--- 6-modal/app.js
+++ 6-modal/app.js
@@ -1,10 +1,9 @@
-const modalBtn = document.querySelector('.btn')
 const overlay = document.querySelector('.modal_overlay')
-const closeBtn = document.querySelector('.close_btn')
 
-modalBtn.addEventListener('click', function(){
+document.querySelector('.btn').addEventListener('click', function(){
     overlay.classList.add('show_modal')
 })
-closeBtn.addEventListener('click', function(){
+
+document.querySelector('.close_btn').addEventListener('click', function(){
     overlay.classList.remove('show_modal')
 })
