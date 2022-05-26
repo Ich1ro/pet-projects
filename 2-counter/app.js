c0f20022ae99c4e15ecdbab896ecd5795f7a4a8e
Ich1ro <pavel.dubenchuk.02@gmail.com>
2022-05-26 19:52:35 +0300

    refactoring

--- 2-counter/app.js
+++ 2-counter/app.js
@@ -4,20 +4,11 @@ const value = document.querySelector('#value')
 
 btns.forEach(function(btn){
     btn.addEventListener('click', function(e){
-        const styles = e.currentTarget.classList
-        if(styles.contains("decrease")){
-            count--
-        }
-        else if(styles.contains("increase")){
-            count++
-        }
-        else{
-            count = 0
-        }
-        count > 0 ? value.style.color = 'green' : value.style.color = 'red'
-        if(count === 0){
-            value.style.color = '#222222'
-        }
+        const payload = e.target.dataset.payload;
+        count = payload ? count + +payload : 0;
+        value.style.color = count > 0 ? 'green' :
+            count < 0 ? 'red' :
+                '#222222';
         value.textContent = count
     })
 })
